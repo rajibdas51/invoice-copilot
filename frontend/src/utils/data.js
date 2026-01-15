6f46495e9ddb11fa24c9b5839e056b9c0eb8e2f1
Rajib chandra Das <prorajib6@gmail.com>
2026-01-16 04:09:15 +0600

    add features data

---
+++ frontend/src/utils/data.js
@@ -0,0 +1,27 @@
+import { BarChart2, FileText, Mail, Sparkles } from "lucide-react";
+
+export const FEATURES =[
+
+    {
+        icon:Sparkles,
+        title:"AI-Powered Invoice Creation",
+        description:"Paste any text, email, or receipt and let our AI instantly generate a complete, professional invoice for you. "
+    },
+    
+    {
+        icon:BarChart2,
+        title:"AI-Powered Dashboard Insights",
+        description:"get smart , actionable insights about your business finances, generated automatically by AI."
+    },
+    {
+        icon:Mail,
+        title:"Smart Payment Reminders",
+        description:"Automatically generate polite and effective payment reminder emails using AI, helping you get paid faster with less effort."
+    },
+     {
+        icon:FileText,
+        title:"Easy Invoice Management",
+        description:"Organize, track, and manage all your invoices in one place with our user-friendly interface designed for efficiency."
+    },
+
+]
