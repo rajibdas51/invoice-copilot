31d072addd4f7baa3dba4ff0a47f7136671a1ec5
Rajib chandra Das <prorajib6@gmail.com>
2026-01-24 15:29:32 +0600

    add FAQS data

--- frontend/src/utils/data.js
+++ frontend/src/utils/data.js
@@ -54,3 +54,33 @@ export const TESTIMONIALS =[
         avatar:"https://randomuser.me/api/portraits/women/68.jpg"
     }
 ]
+
+export const FAQS =[
+    {
+        question:"How does the AI-powered invoice creation work?",
+        answer:"Our AI analyzes the text, email, or receipt you provide and generates a professional invoice by extracting relevant details such as items, prices, and client information."
+    },{
+        question:"Is there free trial available?",
+        answer:"Yes, we offer a 14-day free trial for new users to explore all the features of our AI Invoice Generator without any limitations."
+    },
+    {
+        question:"What is your cancellation policy?",
+        answer:"You can cancel your subscription at any time from your account settings. There are no cancellation fees, and you will retain access to the service until the end of your billing cycle."
+    }
+    ,
+    {
+        question:"Is my data secure with AI Invoice Generator?",
+        answer:"Absolutely. We prioritize your data security and privacy. All data is encrypted in transit and at rest, and we comply with industry-standard security practices to protect your information."   
+    },
+    {
+        question:"Can I change my subscription plan later?",
+        answer:"Yes, you can upgrade or downgrade your subscription plan at any time from your account settings. The changes will take effect immediately, and you will be billed accordingly." 
+    },{
+        question:"Can other info be added to an invoice?",
+        answer:"Yes, our AI Invoice Generator allows you to customize invoices by adding additional information such as logos, payment terms, and notes to suit your business needs."
+    },
+    {
+        question:"What payment methods do you accept?",
+        answer:"We accept all major credit cards, including Visa, MasterCard, American Express, as well as PayPal for your convenience."
+    }
+]
