b7e70203f0885cb96d21c42598674426ef2dde96
Rajib chandra Das <prorajib6@gmail.com>
2026-01-27 22:57:14 +0600

    initialize auth controller

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import {registerUser,loginUser,getMe, updateUserProfile} from '../controllers/authController'
+import protect from '../middlewares/authMiddleware';
+
+const router = express.Router();
+
+
+router.post('/register',registerUser);
+router.post('/login',loginUser);
+router.route('/me').get(protect,getMe).put(protect,updateUserProfile);
+
+export default router;
+
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import {ConnectDB} from './config/db.js';
-
+import authRoutes from './routes/authRoutes.js'
 dotenv.config();
 
 // app config
