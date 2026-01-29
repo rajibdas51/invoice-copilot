81ae70191b991727d0844c2936aeca76f9450ecf
Rajib chandra Das <prorajib6@gmail.com>
2026-01-29 22:27:42 +0600

    create update user controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,11 +1,10 @@
-import jwt from 'jsonwebtoken'
-import User from '../models/User.js'
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
 // Helper: Generate JWT
-const generateToken = (id)=>{
-    return jwt.sign({id},process.env.JWT_SECRET,{expiresIn:"7d"})
-}
-
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+};
 
 // @desc Register new user
 // @route POST /api/auth/register
@@ -14,16 +13,14 @@ const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
-
     res.status(400);
-    throw new Error('Please fill all the fields!');
+    throw new Error("Please fill all the fields!");
   }
-
 
   const userExists = await User.findOne({ email });
   if (userExists) {
     res.status(400);
-    throw new Error('User already exists');
+    throw new Error("User already exists");
   }
 
   const user = await User.create({ name, email, password });
@@ -36,87 +33,94 @@ const registerUser = async (req, res) => {
   });
 };
 
-
-
 // @desc login  user
 // @route POST /api/auth/login
-  
+
 // @access Public
- const loginUser = async (req, res) =>{
-  console.log('req.body:', req.body);
-   const { email,password} = req.body;
+const loginUser = async (req, res) => {
+  console.log("req.body:", req.body);
+  const { email, password } = req.body;
 
-     if ( !email || !password) {
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Please fill all the fields!");
+  }
+  console.log(email, password);
+  try {
+    const user = await User.findOne({ email }).select("+password");
 
-       res.status(400);
-       throw new Error('Please fill all the fields!');
-      }
-   console.log(email,password);
-   try{
-    const user = await User.findOne({email}).select("+password");
-
-    if(user && (await user.matchPassword(password))){
-       res.json({
-        _id:user._id,
-        name:user.name,
-        email:user.email,
-        token:generateToken(user._id),
-        businessName:user.businessName || "",
-        address:user.address || "",
-        phone:user.phone || "",
-       })
-      
-    }else{
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+        businessName: user.businessName || "",
+        address: user.address || "",
+        phone: user.phone || "",
+      });
+    } else {
       res.status(401);
-      throw new Error('Invalid email or password');
+      throw new Error("Invalid email or password");
     }
-     
-   } catch(error){
+  } catch (error) {
     res.status(500);
-    throw new Error('Server error');
-   }
-}
-
-
+    throw new Error("Server error");
+  }
+};
 
 // @desc Get current  logged-in  user
 // @route POST /api/auth/me
-  
+
 // @access Private
 
-const getMe = async (req, res)=>{
-    try{
-      const user = await User.findById(req.user.id)
-      res.json({
-        _id:user._id,
-        name:user.name,
-        email:user.email,
-        businessName:user.businessName || "",
-        address:user.address || "",
-        phone:user.phone || "",
-      })
-   
-   } catch(error){
-       res.status(500);
-       throw new Error('Server error');
-    
-   }
-
-}
-
-
+const getMe = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      businessName: user.businessName || "",
+      address: user.address || "",
+      phone: user.phone || "",
+    });
+  } catch (error) {
+    res.status(500);
+    throw new Error("Server error");
+  }
+};
 
 // @desc Update user profile
 // @route POST /api/auth/update
-  
+
 // @access Private
 
-const updateUserProfile = async (req, res)=>{
-    try{
-   
-   } catch(error){
-       res.status(500).json({message:"Server error"})
-   }
-}
+const updateUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.businessName = req.body.businessName || user.businessName;
+      user.address = req.body.address || user.address;
+      user.phone = req.body.phone || user.phone;
+      const updatedUser = await user.save();
+      res.json({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        businessName: updatedUser.businessName || "",
+        address: updatedUser.address || "",
+        phone: updatedUser.phone || "",
+      });
+    } else {
+      res.status(404);
+      throw new Error("User not found");
+    }
+  } catch (error) {
+    res.status(500);
+    throw new Error("Server error");
+  }
+};
 
-export {registerUser,loginUser,getMe, updateUserProfile};
+export { registerUser, loginUser, getMe, updateUserProfile };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,16 @@
-import express from 'express';
-import {registerUser,loginUser,getMe, updateUserProfile} from '../controllers/authController.js'
-import protect from '../middlewares/authMiddleware.js';
+import express from "express";
+import {
+  registerUser,
+  loginUser,
+  getMe,
+  updateUserProfile,
+} from "../controllers/authController.js";
+import protect from "../middlewares/authMiddleware.js";
 
 const authRouter = express.Router();
 
-
-authRouter.post('/register',registerUser);
-authRouter.post('/login',loginUser);
-authRouter.route('/me').get(protect,getMe).put(protect,updateUserProfile);
+authRouter.post("/register", registerUser);
+authRouter.post("/login", loginUser);
+authRouter.route("/me").get(protect, getMe).put(protect, updateUserProfile);
 
 export default authRouter;
