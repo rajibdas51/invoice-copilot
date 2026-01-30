798d448b09e6c6023ffbc91110898270e29cfe85
Rajib chandra Das <prorajib6@gmail.com>
2026-01-30 13:34:55 +0600

    create createInvoice controller

---
+++ backend/controllers/invoiceController.js
@@ -0,0 +1,92 @@
+import Invoice from "../models/invoiceModel.js";
+
+// @desc    Create a new invoice
+// @route   POST /api/invoices
+// @access  Private
+export const createInvoice = async (req, res) => {
+  try {
+    const user = req.user;
+    const {
+      invoiceNumber,
+      invoiceDate,
+      dueDate,
+      billFrom,
+      billTo,
+      items,
+      notes,
+      paymentTerms,
+    } = req.body;
+
+    // subtotal calculation
+    let subtotal = 0;
+    let taxTotal = 0;
+    items.forEach((item) => {
+      subtotal += item.unitPrice * item.quantity;
+      taxTotal +=
+        (item.unitPrice * item.quantity * (item.taxPercent || 0)) / 100;
+    });
+
+    const total = subtotal + taxTotal;
+
+    const invoice = new Invoice({
+      user,
+      invoiceNumber,
+      invoiceDate,
+      dueDate,
+      billFrom,
+      billTo,
+      items,
+      notes,
+      paymentTerms,
+      subtotal,
+      taxTotal,
+      total,
+    });
+
+    await invoice.save();
+    res.status(201).json(invoice);
+  } catch (error) {
+    res.status(500);
+    throw new Error("Error creating invoice");
+  }
+};
+
+// @desc    Get all invoices for the logged-in user
+// @route   GET /api/invoices
+// @access  Private
+export const getInvoices = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500);
+    throw new Error("Error fetching invoices");
+  }
+};
+
+// @desc    Get a single invoice by ID
+// @route   GET /api/invoices/:id
+// @access  Private
+export const getInvoiceById = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// @desc    Update an invoice
+// @route   PUT /api/invoices/:id
+// @access  Private
+export const updateInvoice = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500);
+    throw new Error("Error updating invoice");
+  }
+};
+
+// @desc    Delete an invoice
+// @route   DELETE /api/invoices/:id
+// @access  Private
+export const deleteInvoice = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500);
+    throw new Error("Error deleting invoice");
+  }
+};
