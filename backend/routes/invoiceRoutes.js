2a80e8445ecce98a2bbdd7be0c5295e9db908f7f
Rajib chandra Das <prorajib6@gmail.com>
2026-01-30 13:09:21 +0600

    create invoice route

---
+++ backend/routes/invoiceRoutes.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+import {
+  createInvoice,
+  getInvoices,
+  getInvoiceById,
+  updateInvoice,
+  deleteInvoice,
+} from "../controllers/invoiceController.js";
+import protect from "../middlewares/authMiddleware.js";
+
+const invoiceRouter = express.Router();
+
+invoiceRouter.post("/", protect, createInvoice).get(protect, getInvoices);
+invoiceRouter
+  .route("/:id")
+  .get(protect, getInvoiceById)
+  .put(protect, updateInvoice)
+  .delete(protect, deleteInvoice);
+
+export default invoiceRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import cors from 'cors';
-import path from 'path';
-import {ConnectDB} from './config/db.js';
-import authRouter from './routes/authRoutes.js'
-import {  errorHandler } from './middlewares/errorMiddleware.js';
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { ConnectDB } from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
+import invoiceRouter from "./routes/invoiceRoutes.js";
+import { errorHandler } from "./middlewares/errorMiddleware.js";
 
 dotenv.config();
 
@@ -20,20 +21,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Middleware to handle CORS (AFTER body parsers)
-app.use(cors({
-    origin:"*",
-    methods:["GET","POST","PUT","DELETE"],
-    allowedHeaders:["Content-Type","Authorization"]
-}));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 // Routes
-app.use("/api/auth",authRouter)
-app.get('/', (req, res) => {
-    res.send("API is running...");
+app.use("/api/auth", authRouter);
+app.use("/api/invoices", invoiceRouter);
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
 // Error Handling Middleware
 app.use(errorHandler);
 
 // start Server
-app.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
