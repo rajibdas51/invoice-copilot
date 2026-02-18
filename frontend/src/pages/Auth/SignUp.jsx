f510577ad01b65abc8ef5286d787c3d66b176256
Rajib chandra Das <prorajib6@gmail.com>
2026-02-18 21:31:56 +0600

    feat: add signup form HTML structure and layout

--- frontend/src/pages/Auth/SignUp.jsx
+++ frontend/src/pages/Auth/SignUp.jsx
@@ -87,8 +87,135 @@ const SignUp = () => {
              )}
           </div>
           {/* Email */}
+          <div>
+            <label className=''>Email</label>
 
+            <div className="">
+              <Mail className='' />
+              <input type="email" 
+               name='email'
+               required
+               value={formData.email}
+               onChange={handleInputChange}
+               onBlur={handleBlur}
+                 className={`w-full pl-12 pr-4 py-3 border rounded-lg focus:ring-2 focus:border-transparent outline-none transition-all ${fieldErrors.email && touched.email ?"border-red-300 focus:ring-red-500":"border-gray-300 focus:ring-black"}`}
+                 placeholder='Enter your email'
+              />
+            </div>
+            {fieldErrors.email && touched.email &&(
+              <p className="">{fieldErrors.email}</p>
+            )}
+
+          </div>
+          {/* Password */}
+          <div>
+            <label className=''>Password</label>
+            <div className="">
+              <Lock className='' />
+              <input type={showPassword?"text":"password"} 
+              required
+              value={formData.password}
+              onChange={handleInputChange}
+              onBlur={handleBlur}
+              className={`w-full pl-12 pr-4 py-3 border rounded-lg focus:ring-2 focus:border-transparent outline-none transition-all ${fieldErrors.password && touched.password ?"border-red-300 focus:ring-red-500":"border-gray-300 focus:ring-black"}`}
+              placeholder='Create a password'
+              />
+              <button type='button' onClick={()=>setShowPassword(!showPassword)} className="">
+                {showPassword ?(<EyeOff className=''/>):(<Eye className=''/>)}
+              </button>
+            </div>
+            {fieldErrors.password && touched.password &&(
+              <p className="">{fieldErrors.password}</p>
+            )}
+          </div>
+          
+          {/* Confirm password*/}
+
+          <div>
+            <label  className="">Confirm Password</label>
+            <div className="">
+              <Lock className=''/>
+              <input type={showConfirmPassword? "text":"password"}
+              required
+              value={formData.confimPassword}
+              onChange={handleInputChange}
+              onBlur={handleBlur}
+
+               className={`w-full pl-12 pr-4 py-3 border rounded-lg focus:ring-2 focus:border-transparent outline-none transition-all ${fieldErrors.confimPassword && touched.confimPassword ?"border-red-300 focus:ring-red-500":"border-gray-300 focus:ring-black"}`}
+              placeholder='Confirm your password'/>
+              <button
+              type='button'
+              onClick={()=>setShowConfirmPassword(!showConfirmPassword)}
+              className="">
+              {
+                showConfirmPassword? (
+                  <EyeOff className='' />
+                ):(<Eye className='' />)
+              }
+              </button>
+            </div>
+            {fieldErrors.confimPassword && touched.confimPassword && (
+              <p className="">{fieldErrors.confimPassword}</p>
+            )}
+          </div>
+
+          {/* Error/success Messages */}
+           {
+            error &&(
+              <div className="">
+                <p className="">{error}</p>
+              </div>
+            )
+           }
+
+           {
+            success &&(
+              <div className="">
+                <p className="">{success}</p>
+              </div>
+            )
+           }
+         
+         {/* Terms & Conditions */}
+
+         <div className="">
+          <input type="checkbox" id='terms'  className="" required/>
+          <label htmlFor="" className=''>
+            I agree to the {" "} 
+            <button className="">Terms of Service</button> {" "}
+            and{" "}
+            <button className="">Privacy Policy</button>
+          </label>
+         </div>
+
+         {/* Sign Up Button */}
+         <button  
+         onClick={handleSubmit} 
+         disabled={isLoading || !isFormValid()}
+         className=""
+         >
+          {
+            isLoading ? (
+              <>
+              <Loader2 className=''/>
+              Creating account...
+              </>
+            ):(<>
+              Create Account 
+              <ArrowRight className='' />
+            </>)
+          }
+         </button>
         </div>
+
+        {/*----- Footer ---*/}
+        <div className="">
+          <p className="">
+            Already have an Account? {" "}
+            <button className="" onClick={()=> navigate("/login")}>Sign in</button>
+          </p>
+        </div>
+
        </div>
     </div>
   )
