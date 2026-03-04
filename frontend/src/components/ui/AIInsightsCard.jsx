ad54a443163265805d7984062b823700cb7cdd91
Rajib chandra Das <prorajib6@gmail.com>
2026-03-04 20:18:03 +0600

    feat: add state variables

--- frontend/src/components/ui/AIInsightsCard.jsx
+++ frontend/src/components/ui/AIInsightsCard.jsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import {useState,useEffect} from 'react';
+import { Lightbulb } from 'lucide-react';
+import axiosInstance from '../../utils/axiosinstance';
+import { API_PATHS } from '../../utils/apiPaths';
 
 const AIInsightsCard = () => {
+  const [insights, setInsights] = useState([]);
+  const[isLoading, setIsLoading] = useState(true);
+  
+  useEffect(()=>{
+    const fetchInsights = async () => {
+      try {
+        const response = await axiosInstance.get(API_PATHS.AI.GET_DASHBOARD_SUMMARY);
+        setInsights(response.data.setInsights || []);
+        
+      } catch (error) {
+        console.error("Error fetching insights:", error);
+      } finally {
+        setIsLoading(false);        
+      }
+    }
+
+    fetchInsights();
+  },[])
+
+
   return (
     <div>AIInsightsCard</div>
   )
