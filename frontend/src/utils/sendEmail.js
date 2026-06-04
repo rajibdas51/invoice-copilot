41da25d3cb254f43c54358c2a56a9f628f1cc072
Rajib Chandra Das <prorajib6@gmail.com>
2026-06-05 00:11:30 +0600

    feat: create sendEmail utility function

---
+++ frontend/src/utils/sendEmail.js
@@ -0,0 +1,21 @@
+import nodemailer from "nodemailer";
+
+const sendEmail = async ({ to, subject, html }) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  await transporter.sendMail({
+    from: `"Invoice Copilot" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    html,
+  });
+};
+
+export default sendEmail;
